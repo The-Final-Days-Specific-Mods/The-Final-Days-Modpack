8164ec32039417da5a61df594910df631336a29e
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2022-08-20 17:14:26 +0800

    Added `Boots Expanded`

---
+++ Contents/mods/Boots Expanded/media/lua/server/items/WC_Distributions.lua
@@ -0,0 +1,263 @@
+require 'Items/ProceduralDistributions'
+
+--BedroomDresser
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Shoes_JungleBoots");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 0.005);
+
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Shoes_GothBoots");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 0.1);
+
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Shoes_ConverseBoots");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 0.2);
+
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Shoes_FancyBoots");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 0.1);
+
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Shoes_StrappedBoots");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 0.3);
+
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Shoes_DrMartens");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 0.3);
+
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Shoes_HikingBoots");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 1);
+
+
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Shoes_Hitops");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 0.5);
+
+
+--DresserGeneric
+table.insert(ProceduralDistributions.list.DresserGeneric.items, "Shoes_JungleBoots");
+table.insert(ProceduralDistributions.list.DresserGeneric.items, 0.005);
+
+table.insert(ProceduralDistributions.list.DresserGeneric.items, "Shoes_GothBoots");
+table.insert(ProceduralDistributions.list.DresserGeneric.items, 0.05);
+
+table.insert(ProceduralDistributions.list.DresserGeneric.items, "Shoes_ConverseBoots");
+table.insert(ProceduralDistributions.list.DresserGeneric.items, 0.05);
+
+
+table.insert(ProceduralDistributions.list.DresserGeneric.items, "Shoes_StrappedBoots");
+table.insert(ProceduralDistributions.list.DresserGeneric.items, 0.2);
+
+table.insert(ProceduralDistributions.list.DresserGeneric.items, "Shoes_FancyBoots");
+table.insert(ProceduralDistributions.list.DresserGeneric.items, 0.05);
+
+table.insert(ProceduralDistributions.list.DresserGeneric.items, "Shoes_DrMartens");
+table.insert(ProceduralDistributions.list.DresserGeneric.items, 0.15);
+
+table.insert(ProceduralDistributions.list.DresserGeneric.items, "Shoes_HikingBoots");
+table.insert(ProceduralDistributions.list.DresserGeneric.items, 0.6);
+
+
+table.insert(ProceduralDistributions.list.DresserGeneric.items, "Shoes_Hitops");
+table.insert(ProceduralDistributions.list.DresserGeneric.items, 0.2);
+
+--WardrobeWoman
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Shoes_GothBoots");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.1);
+
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Shoes_ConverseBoots");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.2);
+
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Shoes_StrappedBoots");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.5);
+
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Shoes_FancyBoots");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.1);
+
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Shoes_DrMartens");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.4);
+
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Shoes_HikingBoots");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.7);
+
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Shoes_BlackBoots");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.2);
+
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Shoes_RidingBoots");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.2);
+
+--WardrobeWomanClassy
+table.insert(ProceduralDistributions.list.WardrobeWomanClassy.items, "Shoes_GothBoots");
+table.insert(ProceduralDistributions.list.WardrobeWomanClassy.items, 0.5);
+
+table.insert(ProceduralDistributions.list.WardrobeWomanClassy.items, "Shoes_ConverseBoots");
+table.insert(ProceduralDistributions.list.WardrobeWomanClassy.items, 0.5);
+
+table.insert(ProceduralDistributions.list.WardrobeWomanClassy.items, "Shoes_StrappedBoots");
+table.insert(ProceduralDistributions.list.WardrobeWomanClassy.items, 0.5);
+
+table.insert(ProceduralDistributions.list.WardrobeWomanClassy.items, "Shoes_FancyBoots");
+table.insert(ProceduralDistributions.list.WardrobeWomanClassy.items, 0.5);
+
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Shoes_BlackBoots");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.5);
+
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Shoes_RidingBoots");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.5);
+
+
+
+--WardrobeMan
+table.insert(ProceduralDistributions.list.WardrobeMan.items, "Shoes_JungleBoots");
+table.insert(ProceduralDistributions.list.WardrobeMan.items, 0.005);
+
+table.insert(ProceduralDistributions.list.WardrobeMan.items, "Shoes_StrappedBoots");
+table.insert(ProceduralDistributions.list.WardrobeMan.items, 0.2);
+
+table.insert(ProceduralDistributions.list.WardrobeMan.items, "Shoes_HikingBoots");
+table.insert(ProceduralDistributions.list.WardrobeMan.items, 0.2);
+
+table.insert(ProceduralDistributions.list.WardrobeMan.items, "Shoes_Hitops");
+table.insert(ProceduralDistributions.list.WardrobeMan.items, 0.3);
+
+
+--WardrobeManClassy
+table.insert(ProceduralDistributions.list.WardrobeMan.items, 0.2);
+
+
+table.insert(ProceduralDistributions.list.WardrobeMan.items, "Shoes_HikingBoots");
+table.insert(ProceduralDistributions.list.WardrobeMan.items, 0.2);
+
+table.insert(ProceduralDistributions.list.WardrobeMan.items, "Shoes_Hitops");
+table.insert(ProceduralDistributions.list.WardrobeMan.items, 0.2);
+
+
+--ClosetShelfGeneric
+table.insert(ProceduralDistributions.list.ClosetShelfGeneric.items, "Shoes_JungleBoots");
+table.insert(ProceduralDistributions.list.ClosetShelfGeneric.items, 0.1);
+
+table.insert(ProceduralDistributions.list.ClosetShelfGeneric.items, "Shoes_GothBoots");
+table.insert(ProceduralDistributions.list.ClosetShelfGeneric.items, 0.5);
+
+table.insert(ProceduralDistributions.list.ClosetShelfGeneric.items, "Shoes_ConverseBoots");
+table.insert(ProceduralDistributions.list.ClosetShelfGeneric.items, 0.5);
+
+table.insert(ProceduralDistributions.list.ClosetShelfGeneric.items, "Shoes_FancyBoots");
+table.insert(ProceduralDistributions.list.ClosetShelfGeneric.items, 0.5);
+
+table.insert(ProceduralDistributions.list.ClosetShelfGeneric.items, "Shoes_DrMartens");
+table.insert(ProceduralDistributions.list.ClosetShelfGeneric.items, 0.5);
+
+table.insert(ProceduralDistributions.list.ClosetShelfGeneric.items, "Shoes_HikingBoots");
+table.insert(ProceduralDistributions.list.ClosetShelfGeneric.items, 1);
+
+table.insert(ProceduralDistributions.list.ClosetShelfGeneric.items, "Shoes_Hitops");
+table.insert(ProceduralDistributions.list.ClosetShelfGeneric.items, 0.5);
+
+
+table.insert(ProceduralDistributions.list.ClosetShelfGeneric.items, "Shoes_StrappedBoots");
+table.insert(ProceduralDistributions.list.ClosetShelfGeneric.items, 1);
+
+--ClothingStoresBoots
+table.insert(ProceduralDistributions.list.ClothingStoresBoots.items, "Shoes_JungleBoots");
+table.insert(ProceduralDistributions.list.ClothingStoresBoots.items, 1);
+
+table.insert(ProceduralDistributions.list.ClothingStoresBoots.items, "Shoes_GothBoots");
+table.insert(ProceduralDistributions.list.ClothingStoresBoots.items, 20);
+
+table.insert(ProceduralDistributions.list.ClothingStoresBoots.items, "Shoes_ConverseBoots");
+table.insert(ProceduralDistributions.list.ClothingStoresBoots.items, 20);
+
+table.insert(ProceduralDistributions.list.ClothingStoresBoots.items, "Shoes_FancyBoots");
+table.insert(ProceduralDistributions.list.ClothingStoresBoots.items, 20);
+
+table.insert(ProceduralDistributions.list.ClothingStoresBoots.items, "Shoes_DrMartens");
+table.insert(ProceduralDistributions.list.ClothingStoresBoots.items, 40);
+
+table.insert(ProceduralDistributions.list.ClothingStoresBoots.items, "Shoes_HikingBoots");
+table.insert(ProceduralDistributions.list.ClothingStoresBoots.items, 40);
+
+table.insert(ProceduralDistributions.list.ClothingStoresBoots.items, "Shoes_StrappedBoots");
+table.insert(ProceduralDistributions.list.ClothingStoresBoots.items, 40);
+
+
+--ClothingStoresShoesLeather
+table.insert(ProceduralDistributions.list.ClothingStoresShoesLeather.items, "Shoes_JungleBoots");
+table.insert(ProceduralDistributions.list.ClothingStoresShoesLeather.items, 0.01);
+
+table.insert(ProceduralDistributions.list.ClothingStoresShoesLeather.items, "Shoes_GothBoots");
+table.insert(ProceduralDistributions.list.ClothingStoresShoesLeather.items, 10);
+
+table.insert(ProceduralDistributions.list.ClothingStoresShoesLeather.items, "Shoes_FancyBoots");
+table.insert(ProceduralDistributions.list.ClothingStoresShoesLeather.items, 10);
+
+table.insert(ProceduralDistributions.list.ClothingStoresShoesLeather.items, "Shoes_DrMartens");
+table.insert(ProceduralDistributions.list.ClothingStoresShoesLeather.items, 10);
+
+table.insert(ProceduralDistributions.list.ClothingStoresShoesLeather.items, "Shoes_HikingBoots");
+table.insert(ProceduralDistributions.list.ClothingStoresShoesLeather.items, 10);
+
+table.insert(ProceduralDistributions.list.ClothingStoresShoesLeather.items, "Shoes_StrappedBoots");
+table.insert(ProceduralDistributions.list.ClothingStoresShoesLeather.items, 10);
+
+
+--CrateClothesRandom
+
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, "Shoes_JungleBoots");
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, 1);
+
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, "Shoes_GothBoots");
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, 2);
+
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, "Shoes_ConverseBoots");
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, 2);
+
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, "Shoes_FancyBoots");
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, 2);
+
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, "Shoes_DrMartens");
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, 4);
+
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, "Shoes_HikingBoots");
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, 5);
+
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, "Shoes_Hitops");
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, 4);
+
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, "Shoes_StrappedBoots");
+table.insert(ProceduralDistributions.list.CrateClothesRandom.items, 4);
+
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Shoes_BlackBoots");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 4);
+
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Shoes_RidingBoots");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 4);
+
+--GymLockers
+table.insert(ProceduralDistributions.list.GymLockers.items, "Shoes_Hitops");
+table.insert(ProceduralDistributions.list.GymLockers.items, 2);
+
+--Locker
+
+table.insert(ProceduralDistributions.list.Locker.items, "Shoes_ConverseBoots");
+table.insert(ProceduralDistributions.list.Locker.items, 0.5);
+
+table.insert(ProceduralDistributions.list.Locker.items, "Shoes_FancyBoots");
+table.insert(ProceduralDistributions.list.Locker.items, 0.5);
+
+table.insert(ProceduralDistributions.list.Locker.items, "Shoes_DrMartens");
+table.insert(ProceduralDistributions.list.Locker.items, 0.5);
+
+table.insert(ProceduralDistributions.list.Locker.items, "Shoes_HikingBoots");
+table.insert(ProceduralDistributions.list.Locker.items, 0.5);
+
+
+table.insert(ProceduralDistributions.list.Locker.items, "Shoes_Hitops");
+table.insert(ProceduralDistributions.list.Locker.items, 0.5);
+
+
+table.insert(ProceduralDistributions.list.Locker.items, "Shoes_StrappedBoots");
+table.insert(ProceduralDistributions.list.Locker.items, 0.5);
+
+--StripClubDressers
+table.insert(ProceduralDistributions.list.StripClubDressers.items, "Shoes_FancyBoots");
+table.insert(ProceduralDistributions.list.StripClubDressers.items, 20);
+
+table.insert(ProceduralDistributions.list.StripClubDressers.items, "Shoes_GothBoots");
+table.insert(ProceduralDistributions.list.StripClubDressers.items, 10);
+
+table.insert(ProceduralDistributions.list.StripClubDressers.items, "Shoes_StrappedBoots");
+table.insert(ProceduralDistributions.list.StripClubDressers.items, 20);
