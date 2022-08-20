65e5ef43b3da2f7ce92a3e07b0302e982bcf1d24
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2022-08-20 17:18:07 +0800

    Revert "Map Additions"
    This reverts commit 7658ac263ea6173c9a3cdb18ef84b0f561a58abd.

--- Contents/mods/Trelai_4x4/media/maps/Trelai_4x4/spawnregions.lua
+++
@@ -1,5 +0,0 @@
-function SpawnRegions()
-    return {
-		{ name = "Trelai Academy,KY", file = "media/maps/Trelai_4x4/spawnpoints.lua" },
-    }
-end
--- Contents/mods/li_township/media/lua/server/items/li_townshipmapDistribution.lua
+++
@@ -1,5 +0,0 @@
-local function preDistributionMerge()
-    table.insert(ProceduralDistributions.list.MagazineRackMaps.items, "li_townshipmap");
-    table.insert(ProceduralDistributions.list.MagazineRackMaps.items, 50);
-end
-Events.OnPreDistributionMerge.Add(preDistributionMerge); 
